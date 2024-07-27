c70d4caf7376c759cc2edee8425476833446a8ee
Loic Tatchum <tatchumdilane98@gmail.com>
2024-07-27 20:09:54 +0400

    Next.js portfolio

---
+++ data/index.ts
@@ -0,0 +1,198 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently contributing to the development of a web application",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b6.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Online chat app",
+      des: "Explore the activities of your loved ones to discover magnificent places.",
+      img: "/discu.svg",
+      iconLists: ["/re.svg", "/css.svg", "/js.svg"],
+      link: "https://github.com/Loic-bg/Discussion",
+    },
+    {
+      id: 2,
+      title: "Delivered Foods App",
+      des: "Order from top local restaurants, track your delivery, and enjoy your favorite food at your doorstep.",
+      img: "/pp2 1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg"],
+      link: "https://github.com/Loic-bg/food-app",
+    },
+    {
+      id: 3,
+      title: "e-commerce Webside",
+      des: "Discover a world of products at your fingertips! Browse top brands, explore curated collections, and shop with ease. Get exclusive deals, track your orders, and enjoy seamless checkout.",
+      img: "/e-commerce.svg",
+      iconLists: ["/re.svg", "/css.svg", "/js.svg"],
+      link: "https://github.com/Loic-bg/e-commerce",
+    },
+    {
+      id: 4,
+      title: "Online School Education",
+      des: "Access educational resources, interactive lessons, and personalized learning paths. Connect with teachers, collaborate with classmates, and track your progress.",
+      img: "/school.svg",
+      iconLists: ["/re.svg", "/css.svg", "/js.svg"],
+      link: "https://github.com/Loic-bg/Education-project",
+    },
+    {
+      id: 4,
+      title: "Search Work website",
+      des: "Website to find work, regardless of specialty and field.",
+      img: "/tt.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+      link: "https://talent-tally.web.app",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with you was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Loic's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Loic is the ideal partner.",
+      name: "Michael",
+      title: "Director of Golden",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "individual entrepreneurship",
+      desc: "Layout designer, Front-End developer, Workbench and landing pages development on React.js.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Freelance Up-Work",
+      desc: " Designed and developed website e-commerce platforms using React.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Website Project",
+      desc: "The company sells drones. created the main page with a new design, added registration with onboarding",
+      className: "md:col-span-2", 
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      links: "https://github.com/Loic-bg"
+    },
+    {
+      id: 2,
+      img: "/link.svg",
+    },
+  ];
